2dbeae9fe74d4f1818d1cf43bed645b3f86db76c
Zulma <zumendez7@gmail.com>
2021-04-14 15:40:39 +0200

    structure du dossier - création navbar et footer

---
+++ resources/views/partial/nav.blade.php
@@ -0,0 +1,32 @@
+<header id="masthead" class="site-header site-header-fill">
+
+    <nav id="primary-navigation" class="site-navigation">
+
+        <div class="container-fluid">
+
+            <div class="navbar-header">
+                
+                <a href="index.html" class="site-title yellow-text">{{env('APP_NAME')}}</a>
+
+            </div><!-- /.navbar-header -->
+
+            <div class="main-menu" id="perfect-navbar-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li >
+                        <a href="{{route('home')}}">Home</a></li>
+                    <li class="active">
+                        <a href="{{route('blog')}}">Blog </a>
+                    </li>
+                    <li>
+                        <a href="{{route('portfolio')}}">Portfolio</a>                                
+                    </li>
+                    <li>
+                        <a href="{{route('contact')}}">Contact</a>
+                    </li> 
+                </ul><!-- /.navbar-nav -->
+            </div><!-- /.navbar-collapse -->
+            
+        </div>
+    </nav><!-- /.site-navigation -->
+    
+</header><!-- /#masthead -->
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PortfolioController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/blog', [BlogController::class, 'index'])->name('blog');
+
+Route::get('/portfolio', [PortfolioController::class, 'index'])->name('portfolio');
+
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
