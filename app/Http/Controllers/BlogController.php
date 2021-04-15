778ce70590fe6f7aa937b1e18b76af4ddda15148
Zulma <zumendez7@gmail.com>
2021-04-15 10:04:54 +0200

    page blog terminé

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    //
+    public function index(){
+        $blog = Article::all();
+        return view('pages/blog', compact('blog'));
+    }
 }
--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Projet;
 use Illuminate\Http\Request;
 
 class PortfolioController extends Controller
 {
-    //
+    public function index(){
+        $portfolio = Projet::all();
+        return view('pages/portfolio', compact('portfolio'));
+    }
 }
